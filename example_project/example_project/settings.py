fef84d2ec9b1d09d1855e1bb4ddd7c3594f8eb00
Yasser Toruno <yasser@yassers-mbp.lan>
2026-06-29 22:59:32 -0400

    add dj-control-room-base to the party

--- example_project/example_project/settings.py
+++ example_project/example_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "dj_control_room_base",
     "dj_signals_panel",
     "example_project",  # For management commands
     "app",
